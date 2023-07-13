0198eb7243d37541ea841b04e3e22d5218cc54c5
adamdiakite <diakite.adam.42@gmail.com>
2023-07-13 10:36:10 +0200

    TT changes

--- GenerateNPY.py
+++ GenerateNPY.py
@@ -8,17 +8,17 @@ def normalization_img(image):
     return (image - np.mean(image)) / np.std(image)
 
 
-label = np.load('/home/adamdiakite/Documents/lungegfr-master/Sampledata/label.npy')
+label = np.load('/home/lito/PycharmProjects/lungegfr/Sampledata/label.npy')
 
-ct_files = os.listdir('/home/adamdiakite/Documents/lungegfr-master/Sampledata/Images_Curie/CT')[2:]
+ct_files = os.listdir('/home/lito/PycharmProjects/lungegfr/Sampledata/Images_Curie/CT')[2:]
 num_files = len(ct_files)
 cts = np.empty((num_files, 64, 64), dtype=np.float64)
 pets = np.empty((num_files, 64, 64), dtype=np.float64)
 fuses = np.empty((num_files, 64, 64), dtype=np.float64)
 
 for i, file in enumerate(ct_files):
-    ct_path = os.path.join('/home/adamdiakite/Documents/lungegfr-master/Sampledata/Images_Curie/CT', file)
-    pet_path = os.path.join('/home/adamdiakite/Documents/lungegfr-master/Sampledata/Images_Curie/PET', file)
+    ct_path = os.path.join('/home/lito/PycharmProjects/lungegfr/Sampledata/Images_Curie/CT', file)
+    pet_path = os.path.join('/home/lito/PycharmProjects/lungegfr/Sampledata/Images_Curie/PET', file)
 
     ct_image = np.array(Image.open(ct_path), dtype=np.float64)
     pet_image = np.array(Image.open(pet_path), dtype=np.float64)
@@ -46,6 +46,6 @@ cts = np.transpose(cts, (0, 2, 1))
 pets = np.transpose(pets, (0, 2, 1))
 fuses = np.transpose(fuses, (0, 2, 1))
 
-np.save('/home/adamdiakite/Documents/lungegfr-master/Sampledata/xpsamplect.npy', cts)
-np.save('/home/adamdiakite/Documents/lungegfr-master/Sampledata/xpsamplepet.npy', pets)
-np.save('/home/adamdiakite/Documents/lungegfr-master/Sampledata/xpsamplefuse.npy', fuses)
+np.save('/home/lito/PycharmProjects/lungegfr/Sampledata/xpsamplect.npy', cts)
+np.save('/home/lito/PycharmProjects/lungegfr/Sampledata/xpsamplepet.npy', pets)
+np.save('/home/lito/PycharmProjects/lungegfr/Sampledata/xpsamplefuse.npy', fuses)
